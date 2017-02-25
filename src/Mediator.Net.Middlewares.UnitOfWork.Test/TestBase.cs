f3d7a4427aa68611934837a44192dbd1e46f8471
Eddy Ma <eddy.ma@smsmt.com>
2017-02-25 14:50:53 +1000

    Refactoring

--- src/Mediator.Net.Middlewares.UnitOfWork.Test/TestBase.cs
+++ src/Mediator.Net.Middlewares.UnitOfWork.Test/TestBase.cs
@@ -16,23 +16,6 @@ namespace Mediator.Net.Middlewares.UnitOfWork.Test
         [OneTimeSetUp]
         public void Setup()
         {
-            var dbFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "UnitOfWork.mdf");
-            var dbLogFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "UnitOfWork_log.ldf");
-            try
-            {
-                File.Delete(dbFile);
-                File.Delete(dbLogFile);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-
-            var fromdbFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory.ToUpper().Replace("BIN\\DEBUG", ""), "APP_Data\\UnitOfWork.mdf");
-            var fromdbLogFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory.ToUpper().Replace("BIN\\DEBUG", ""), "APP_DATA\\UnitOfWork_log.ldf");
-            File.Copy(fromdbFile, dbFile);
-            File.Copy(fromdbLogFile, dbLogFile);
-
             ContainerBuilder = new ContainerBuilder();
             ContainerBuilder.RegisterType<MyDbContext>();
         }
